8ab45ffddec426c51229b555a3f2a5594cf0adfe
DmitryLorents <zmey.task@gmail.com>
2023-02-04 23:21:22 +0600

    CoreDataManager created. It's functionality migrated from AppDelegate Customer.init() created SceneDelegate - saveContext fixed

--- CoreData(Learning)/AppDelegate.swift
+++ CoreData(Learning)/AppDelegate.swift
@@ -14,11 +14,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let entityDescription = NSEntityDescription.entity(forEntityName: "Customer", in: viewContext)
-        let managedObject = NSManagedObject(entity: entityDescription!, insertInto: viewContext)
-        managedObject.setValue("MDI2B LLC", forKey: "name")
-        let name = managedObject.value(forKey: "name")
-        print("Name: \(name ?? "n/a")")
+        let managedObject = Customer()
+        managedObject.name = "OOO AvtoVaz"
+        let name = managedObject.name
+        print("Name: \(name ?? "no data")")
+        
+
+        //describe Entity
+//        let entityDescription = NSEntityDescription.entity(forEntityName: "Customer", in: viewContext)
+//
+//        //create newObject
+//        let managedObject = NSManagedObject(entity: entityDescription!, insertInto: viewContext)
+//
+//        // set attribute to context
+//        managedObject.setValue("MDI2B LLC", forKey: "name")
+//
+//        //read attribute from context
+//        let name = managedObject.value(forKey: "name")
+//        print("Name: \(name ?? "n/a")")
+//
+//        //save object
+//        saveContext()
+//
+//        //getting data from storage
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Customer")
+//        do {
+//            let results = try self.viewContext.fetch(fetchRequest)
+//            for result in results as! [Customer] {
+//                print("Name-\(result.name ?? "no data")")
+//            }
+//        } catch {
+//            print(error)
+//        }
+        
         return true
     }
 
@@ -36,50 +64,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    // MARK: - Core Data stack
-    lazy var viewContext = persistentContainer.viewContext
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "CoreData_Learning_")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+    func saveContext() {
+        CoreDataManager.instance.saveContext()
     }
-
 }
 
--- Customer+CoreDataClass.swift
+++ Customer+CoreDataClass.swift
@@ -11,5 +11,13 @@ import CoreData
 
 
 public class Customer: NSManagedObject {
-
+    
+    convenience init() {
+        
+        let context = CoreDataManager.instance.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Customer", in: context)
+        self.init(entity: entity!, insertInto: context)
+        
+    }
+    
 }
