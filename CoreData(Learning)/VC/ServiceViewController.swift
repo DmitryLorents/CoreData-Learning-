770ace06ee1b564a662a80e1232253340f795cd6
DmitryLorents <zmey.task@gmail.com>
2023-02-06 21:54:58 +0600

    VC folder added Services views added ServiceViewController configured

---
+++ CoreData(Learning)/VC/ServiceViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ServiceViewController.swift
+//  CoreData(Learning)
+//
+//  Created by Mac on 06.02.2023.
+//
+
+import UIKit
+
+class ServiceViewController: UIViewController {
+    
+    var service: Services?
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var infoTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let service = service {
+            nameTF.text = service.name
+            infoTF.text = service.info
+        }
+        
+    }
+    
+    func saveService() -> Bool {
+        
+        if nameTF.text?.isEmpty ?? true {
+            let alert = UIAlertController(title: "Validation error", message: "Please enter name of service", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            present(alert, animated: true)
+            return false
+        }
+        
+        if service == nil {
+            service = Services()
+            service?.name = nameTF.text
+            service?.info = infoTF.text
+            CoreDataManager.instance.saveContext()
+        }
+        return true
+    }
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+        if saveService() {
+        dismiss(animated: true)
+        }
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+        
+        dismiss(animated: true)
+    }
+    
+}
---
+++ CoreData(Learning)/VC/ServicesTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ServicesTableViewController.swift
+//  CoreData(Learning)
+//
+//  Created by Mac on 06.02.2023.
+//
+
+import UIKit
+import CoreData
+
+class ServicesTableViewController: UITableViewController {
+    
+    let fetchedResulltController = CoreDataManager.instance.fetchedResultController(entityName: .services, keyForSort: "name")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            try fetchedResulltController.performFetch()
+        } catch {
+            print(error)
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    //MARK: - BarButtonAction
+    
+    @IBAction func addServiceAction(_ sender: Any) {
+        performSegue(withIdentifier: "ServicesToService", sender: nil)
+    }
+    
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = fetchedResulltController.sections {
+            return sections[section].numberOfObjects
+        } else {return 0}
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        if let service = fetchedResulltController.object(at: indexPath) as? Services {
+            cell.textLabel?.text = service.name
+            cell.detailTextLabel?.text = service.info ?? ""
+        }
+        return cell
+    }
+  
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
