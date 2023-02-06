f683d78956f2dbc46ed8008d101e1eabe4a6b1cf
DmitryLorents <zmey.task@gmail.com>
2023-02-06 17:50:57 +0600

    CustomerViewController has filfilled

--- CoreData(Learning)/CustomerViewController.swift
+++ CoreData(Learning)/CustomerViewController.swift
@@ -11,19 +11,48 @@ class CustomerViewController: UIViewController {
     
     @IBOutlet weak var nameTF: UITextField!
     @IBOutlet weak var infoTF: UITextField!
-    
+    var customer: Customer?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // reading object
+        if let customer = customer {
+            nameTF.text = customer.name
+            infoTF.text = customer.info
+        }
        
     }
     
+    func saveCustomer() -> Bool {
+        //validation of required field
+        if (nameTF.text?.isEmpty) ?? true {
+            let alert = UIAlertController(title: "Validation error", message: "Input the name of customer", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            present(alert, animated: true)
+            return false
+        }
+        //creating object
+        if customer == nil {
+            customer = Customer()
+        }
+        //saving object
+        if let customer = customer {
+            customer.name = nameTF.text
+            customer.info = infoTF.text
+            CoreDataManager.instance.saveContext()
+        }
+        return true
+    }
+    
     @IBAction func saveButtonaction(_ sender: Any) {
+        if saveCustomer() {
+            dismiss(animated: true)
+        }
     }
     
     @IBAction func cancelButtonAction(_ sender: Any) {
+        dismiss(animated: true)
     }
     
 
