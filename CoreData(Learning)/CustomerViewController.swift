01cfbcca7d745728aa8fc95f72d02b0ad262c0d6
DmitryLorents <zmey.task@gmail.com>
2023-02-06 17:19:31 +0600

    A customer view added

---
+++ CoreData(Learning)/CustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerViewController.swift
+//  CoreData(Learning)
+//
+//  Created by Mac on 06.02.2023.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var infoTF: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func saveButtonaction(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+    }
+    
+
+}
