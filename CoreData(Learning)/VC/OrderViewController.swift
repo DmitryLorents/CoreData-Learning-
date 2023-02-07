40c257558d49c6a9518ad744893ea74c5757073c
DmitryLorents <zmey.task@gmail.com>
2023-02-07 22:42:17 +0600

    saveOrder function established object creation functionality added

--- CoreData(Learning)/VC/OrderViewController.swift
+++ CoreData(Learning)/VC/OrderViewController.swift
@@ -19,11 +19,30 @@ class OrderViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        //creating object
+        if order == nil {
+            order = Order()
+            order?.date = Date()
+        }
+        
+        if let order = order {
+            datePicker.date = order.date ?? Date()
+            switchMade.isOn = order.made
+            switchPade.isOn = order.paid
+            customerTF.text = order.customer?.name
+        }
 
         // Do any additional setup after loading the view.
     }
     
     func  saveOrder() -> Bool {
+        if let order = order {
+            order.date = datePicker.date
+            order.made = switchMade.isOn
+            order.paid = switchPade.isOn
+            CoreDataManager.instance.saveContext()
+            return true
+        }
         return false
     }
     
@@ -34,7 +53,7 @@ class OrderViewController: UIViewController {
     }
     
 
-    @IBAction func cancelButtonaction(_ sender: Any) {
+    @IBAction func cancelButtonAction(_ sender: Any) {
         dismiss(animated: true)
     }
     
