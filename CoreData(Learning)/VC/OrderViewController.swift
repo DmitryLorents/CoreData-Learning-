6d709aae309c83ec5c4cc73087ae11b05412c5ab
DmitryLorents <zmey.task@gmail.com>
2023-02-07 10:52:16 +0600

    - order related views established

---
+++ CoreData(Learning)/VC/OrderViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OrderViewController.swift
+//  CoreData(Learning)
+//
+//  Created by Mac on 07.02.2023.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+    }
+    
+
+    @IBAction func cancelButtonaction(_ sender: Any) {
+    }
+    
+}
