fd57e9f9696f6962af121895ac5b062bdef64aec
DmitryLorents <zmey.task@gmail.com>
2023-02-07 21:59:57 +0600

    storyboard some issues fixed > button added Customer TF disabled

--- CoreData(Learning)/VC/OrderViewController.swift
+++ CoreData(Learning)/VC/OrderViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class OrderViewController: UIViewController {
     
     var order: Order?
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var customerTF: UITextField!
+    @IBOutlet weak var switchMade: UISwitch!
+    @IBOutlet weak var switchPade: UISwitch!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
