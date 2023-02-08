333a7d7c1e58c14a82a2a9aa27a8e25fc9b7aa12
DmitryLorents <zmey.task@gmail.com>
2023-02-08 21:33:15 +0600

    segue added RoOfOrderViewController created

---
+++ CoreData(Learning)/VC/RowOfOrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RowOfOrderViewController.swift
+//  CoreData(Learning)
+//
+//  Created by Mac on 08.02.2023.
+//
+
+import UIKit
+
+class RowOfOrderViewController: UIViewController {
+
+    @IBOutlet weak var serviceTF: UITextField!
+    @IBOutlet weak var sumTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func getServiceAction(_ sender: Any) {
+    }
+}
