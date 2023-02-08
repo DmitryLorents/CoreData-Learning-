1dcf5e83e2bf6049aabd95cf2108acd17d979966
DmitryLorents <zmey.task@gmail.com>
2023-02-08 16:16:53 +0600

    added  class func getRowsOfOrder(order: Order) -> NSFetchedResultsController<NSFetchRequestResult>

--- Order+CoreDataClass.swift
+++ Order+CoreDataClass.swift
@@ -11,6 +11,17 @@ import CoreData
 
 
 public class Order: NSManagedObject {
+    
+    class func getRowsOfOrder(order: Order) -> NSFetchedResultsController<NSFetchRequestResult> {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: EntityNames.rowOfOrder.rawValue)
+        let sortDescriptor = NSSortDescriptor(key: "service.name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        let predicate = NSPredicate(format: "%K == %@", "order", order)
+        fetchRequest.predicate = predicate
+        let fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataManager.instance.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        return fetchedResultController
+    }
+    
     convenience init() {
         let coreDataManager = CoreDataManager.instance
         self.init(entity: coreDataManager.entityForName(name: .order), insertInto: coreDataManager.viewContext)
